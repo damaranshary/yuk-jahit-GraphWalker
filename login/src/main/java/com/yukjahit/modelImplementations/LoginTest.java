0ff4a11802e028a925849f571e0a8e78e3c35cde
damaranshary <damar.galihanshary7@gmail.com>
2023-06-22 19:28:35 +0700

    adding the Register and Updating the Login Java Class

--- login/src/main/java/com/yukjahit/modelImplementations/LoginTest.java
+++ login/src/main/java/com/yukjahit/modelImplementations/LoginTest.java
@@ -1,7 +1,6 @@
 package com.yukjahit.modelImplementations;
 
-import com.yukjahit.Login;
-
+import com.yukjahit.*;
 import org.graphwalker.core.machine.ExecutionContext;
 import org.graphwalker.java.annotation.BeforeExecution;
 import org.graphwalker.java.annotation.GraphWalker;
@@ -15,31 +14,44 @@ import java.time.Duration;
 @GraphWalker()
 public class LoginTest extends ExecutionContext implements Login {
 
-    private boolean failedLogin = false;
+    public static boolean failedLogin = false;
+    public static String activeName, activeEmail;
 
-    private WebDriver driver;
+    public static WebDriver driver;
+
+    public static void loginMethod (WebDriver driver, String email, String password) {
+        WebElement emailInput = driver.findElement(By.id("login-email-input"));
+        WebElement passwordInput = driver.findElement(By.id("login-password-input"));
+
+        emailInput.clear();
+        passwordInput.clear();
+
+        emailInput.sendKeys(email);
+        passwordInput.sendKeys(password);
+        driver.findElement(By.id("login-submit-button")).click();
+    }
 
     @BeforeExecution
     public void start() {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\damar\\Documents\\SKRIPSI\\Tools\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.get("http://localhost:5173/");
+        driver.get("http://localhost:4173/");
         driver.manage().window().setSize(new Dimension(974, 1080));
     }
 
     @Override
-    public void v_Home_LoggedIn() {
-        driver.findElement(By.xpath("//*[contains(text(), 'Login Berhasil')]")).isDisplayed();
+    public void v_Home_LoggedIn_SHARED() {
+        String loginButton = driver.findElement(By.id("nav-cart-button")).getText();
+        String registerButton = driver.findElement(By.xpath("//button[@name='nav-account-dropdown']")).getText();
 
-        String LoginButton = driver.findElement(By.id("nav-cart-button")).getText();
-        String RegisterButton = driver.findElement(By.xpath("//button[@name='nav-account-dropdown']")).getText();
+        Assert.assertEquals("Keranjang", loginButton);
+        Assert.assertEquals("Akun", registerButton);
+    }
 
-        Assert.assertEquals("Keranjang", LoginButton);
-        Assert.assertEquals("Akun", RegisterButton);
-
-        //String ToastLoginText = Toast.getText();
-        // Assert.assertEquals(ToastLoginText, "Login Berhasil");
+    @Override
+    public void v_Notification_LoggedIn() {
+        driver.findElement(By.xpath("//*[contains(text(), 'Kamu sudah login')]")).isDisplayed();
     }
 
     @Override
@@ -55,84 +67,89 @@ public class LoginTest extends ExecutionContext implements Login {
     }
 
     @Override
-    public void e_BackToHome() {
-        driver.navigate().back();
+    public void e_To_HomePage() {
+        driver.findElement(By.id("logo-link")).click();
+    }
+
+    @Override
+    public void e_To_ProtectedPages() {
+        driver.get("http://localhost:4173/profile");
+        driver.findElement(By.xpath("//*[contains(text(), 'Silahkan login terlebih dahulu')]")).isDisplayed();
     }
 
     @Override
     public void v_LoginPage() {
-        boolean EmailLabel = driver.findElement(By.xpath("//label[contains(text(), 'Email')]")).isDisplayed();
-        boolean EmailInput = driver.findElement(By.id("login-email-input")).isDisplayed();
-        boolean PasswordLabel = driver.findElement(By.xpath("//label[contains(text(), 'Password')]")).isDisplayed();
-        boolean PasswordInput = driver.findElement(By.id("login-password-input")).isDisplayed();
+        boolean emailLabel = driver.findElement(By.xpath("//label[contains(text(), 'Email')]")).isDisplayed();
+        boolean emailInput = driver.findElement(By.id("login-email-input")).isDisplayed();
+        boolean passwordLabel = driver.findElement(By.xpath("//label[contains(text(), 'Password')]")).isDisplayed();
+        boolean passwordInput = driver.findElement(By.id("login-password-input")).isDisplayed();
 
-        Assert.assertTrue(EmailLabel);
-        Assert.assertTrue(EmailInput);
-        Assert.assertTrue(PasswordLabel);
-        Assert.assertTrue(PasswordInput);
+        Assert.assertTrue(emailLabel);
+        Assert.assertTrue(emailInput);
+        Assert.assertTrue(passwordLabel);
+        Assert.assertTrue(passwordInput);
 
-        if (failedLogin) {
+        /*if (failedLogin) {
             driver.findElement(By.xpath("//*[contains(text(), 'Login Gagal')]")).isDisplayed();
-        }
+        }*/
     }
 
     @Override
-    public void e_LoginWithValidCredentials() {
-        WebElement EmailInput = driver.findElement(By.id("login-email-input"));
-        WebElement PasswordInput = driver.findElement(By.id("login-password-input"));
+    public void e_To_RegisterPage() {
+        driver.findElement(By.id("nav-register-button")).click();
+    }
 
-        EmailInput.clear();
-        PasswordInput.clear();
+    @Override
+    public void e_To_LoginPage_LoggedIn() {
+        driver.get("http://localhost:4173/login");
+        driver.findElement(By.xpath("//*[contains(text(), 'Kamu sudah login')]")).isDisplayed();
+    }
 
-        EmailInput.sendKeys("user@gmail.com");
-        PasswordInput.sendKeys("user1234");
-        driver.findElement(By.id("login-submit-button")).click();
-
+    @Override
+    public void e_Login_With_ValidCredentials() {
+        loginMethod(driver, "user@gmail.com", "user1234");
         failedLogin = false;
+        activeName = "Usery Tadd";
+        activeEmail = "user@gmail.com";
     }
 
     @Override
     public void v_Home_NotLoggedIn() {
-       String LoginButton = driver.findElement(By.id("nav-login-button")).getText();
-       String RegisterButton = driver.findElement(By.id("nav-register-button")).getText();
+       String loginButton = driver.findElement(By.id("nav-login-button")).getText();
+       String registerButton = driver.findElement(By.id("nav-register-button")).getText();
 
-       Assert.assertEquals("Masuk", LoginButton);
-       Assert.assertEquals("Daftar", RegisterButton);
+       Assert.assertEquals("Masuk", loginButton);
+       Assert.assertEquals("Daftar", registerButton);
     }
 
     @Override
-    public void e_Back() {
-        driver.navigate().back();
+    public void e_NoAction() {
+
     }
 
     @Override
-    public void e_ToLoginPage() {
+    public void v_RegisterPage_SHARED() {
+
+    }
+
+    @Override
+    public void e_To_LoginPage() {
         driver.findElement(By.id("nav-login-button")).click();
     }
 
     @Override
-    public void e_LoginWithInvalidCredentials() {
-        WebElement EmailInput = driver.findElement(By.id("login-email-input"));
-        WebElement PasswordInput = driver.findElement(By.id("login-password-input"));
-
-        EmailInput.sendKeys("user@gmail.com");
-        PasswordInput.sendKeys("usersssss");
-
-        driver.findElement(By.id("login-submit-button")).click();
-
-        EmailInput.clear();
-        PasswordInput.clear();
-
+    public void e_Login_With_InvalidCredentials() {
+        loginMethod(driver, "user@gmail.com", "user2345");
         failedLogin = true;
     }
 
     @Override
     public void v_ProfilePage() {
         driver.findElement(By.xpath("//h2[contains(text(), 'Profil Saya')]")).isDisplayed();
-        String Username = driver.findElement(By.id("user-name")).getText();
-        String Email = driver.findElement(By.id("user-email")).getText();
+        String userName = driver.findElement(By.id("user-name")).getText();
+        String userEmail = driver.findElement(By.id("user-email")).getText();
 
-        Assert.assertEquals("Usery Tadd", Username);
-        Assert.assertEquals("user@gmail.com", Email);
+        Assert.assertEquals(activeName, userName);
+        Assert.assertEquals(activeEmail, userEmail);
     }
 }
---
+++ login/src/main/java/com/yukjahit/modelImplementations/RegisterTest.java
@@ -0,0 +1,117 @@
+package com.yukjahit.modelImplementations;
+
+import com.yukjahit.*;
+import org.graphwalker.core.machine.ExecutionContext;
+
+import org.graphwalker.java.annotation.GraphWalker;
+import org.openqa.selenium.*;
+//import org.junit.Assert;
+
+@GraphWalker
+public class RegisterTest extends ExecutionContext implements Register {
+    private int alreadyRegistered = 0;
+
+    public static void registerMethod(WebDriver driver, String name, String email, String password, String confirmPassword, String phone, String address) {
+        driver.findElement(By.id("register-name-input")).sendKeys(name);
+        driver.findElement(By.id("register-email-input")).sendKeys(email);
+        driver.findElement(By.id("register-password-input")).sendKeys(password);
+        driver.findElement(By.id("register-confirm-password-input")).sendKeys(confirmPassword);
+        driver.findElement(By.id("register-phone-input")).sendKeys(phone);
+        driver.findElement(By.id("register-address-input")).sendKeys(address);
+        driver.findElement(By.id("register-submit-button")).click();
+    }
+
+    @Override
+    public void e_Register_With_RegisteredEmail() {
+        registerMethod(LoginTest.driver, "Registered Email", "user@gmail.com", "usertest", "usertest",
+                "08123994588", "Address of Registered Email");
+    }
+
+    @Override
+    public void v_RegisterPage_SHARED() {
+        LoginTest.driver.findElement(By.xpath("//h2[contains(text(), 'Daftar')]")).isDisplayed();
+        LoginTest.driver.findElement(By.xpath("//label[contains(text(), 'Nama')]")).isDisplayed();
+        LoginTest.driver.findElement(By.id("register-name-input")).isDisplayed();
+
+        LoginTest.driver.findElement(By.xpath("//label[contains(text(), 'Alamat Email')]")).isDisplayed();
+        LoginTest.driver.findElement(By.id("register-email-input")).isDisplayed();
+
+        LoginTest.driver.findElement(By.xpath("//label[contains(text(), 'Password')]")).isDisplayed();
+        LoginTest.driver.findElement(By.id("register-password-input")).isDisplayed();
+
+        LoginTest.driver.findElement(By.xpath("//label[contains(text(), 'Konfirmasi Password')]")).isDisplayed();
+        LoginTest.driver.findElement(By.id("register-confirm-password-input")).isDisplayed();
+
+        LoginTest.driver.findElement(By.xpath("//label[contains(text(), 'Nomor HP')]")).isDisplayed();
+        LoginTest.driver.findElement(By.id("register-phone-input")).isDisplayed();
+
+        LoginTest.driver.findElement(By.xpath("//label[contains(text(), 'Alamat Lengkap')]")).isDisplayed();
+        LoginTest.driver.findElement(By.id("register-address-input")).isDisplayed();
+
+//        Assert.assertTrue(NameLabel);
+//        Assert.assertTrue(NameInput);
+//        Assert.assertTrue(EmailLabel);
+//        Assert.assertTrue(EmailInput);
+//        Assert.assertTrue(PasswordLabel);
+//        Assert.assertTrue(PasswordInput);
+//        Assert.assertTrue(ConfirmPasswordLabel);
+//        Assert.assertTrue(ConfirmPasswordInput);
+//        Assert.assertTrue(PhoneLabel);
+//        Assert.assertTrue(PhoneInput);
+//        Assert.assertTrue(AddressLabel);
+//        Assert.assertTrue(AddressInput);
+    }
+
+    @Override
+    public void e_NoAction() {
+
+    }
+
+    @Override
+    public void e_Register_With_WrongPasswordType() {
+        registerMethod(LoginTest.driver, "Wrong Password", "wrongpassword@gmail.com", "password", "password",
+                "08123994588", "Address of Wrong Password");
+    }
+
+    @Override
+    public void v_Home_LoggedIn_SHARED() {
+
+    }
+
+    @Override
+    public void v_RegisterSuccess() {
+        if (alreadyRegistered == 1) {
+            LoginTest.driver.findElement(By.xpath("//*[contains(text(), 'Registrasi Berhasil')]")).isDisplayed();
+        }
+    }
+
+    @Override
+    public void e_Register_With_EmptyField() {
+        LoginTest.driver.findElement(By.id("register-submit-button")).click();
+    }
+
+    @Override
+    public void v_RegisterFailed() {
+        LoginTest.driver.findElement(By.xpath("//*[contains(text(), 'Registrasi Gagal')]")).isDisplayed();
+    }
+
+    @Override
+    public void e_Register_With_ValidData() {
+        if (alreadyRegistered == 0) {
+            registerMethod(LoginTest.driver, "YukJahit MBT (11)", "yukjahit11@email.com", "yukjahitmbt", "yukjahitmbt",
+                    "08123456789", "Address of YukJahit (Valid Data)");
+        }
+        alreadyRegistered++;
+    }
+
+    @Override
+    public void e_Login_With_RegisteredData() {
+        if (alreadyRegistered > 1) {
+            LoginTest.driver.findElement(By.id("nav-login-button")).click();
+        }
+        LoginTest.loginMethod(LoginTest.driver,"yukjahit6@email.com", "yukjahitmbt" );
+        LoginTest.failedLogin = false;
+        LoginTest.activeName = "YukJahit MBT (6)";
+        LoginTest.activeEmail = "yukjahit6@email.com";
+    }
+}
